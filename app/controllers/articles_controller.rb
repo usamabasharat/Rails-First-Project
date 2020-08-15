e2648142cefec77367b7910b5b6453b52a2fa0a5
usama <usaama.ba@gmail.com>
2020-08-15 16:54:48 +0500

    Add partials, destroy, and arranged code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+def index
+  @articles = Article.all
+end
+
 def new
   @article = Article.new
 end
@@ -26,11 +30,18 @@ def update
     redirect_to article_path(@article)
   else
     render 'edit'
-  end 
+  end
 end
 
 def show
   @article = Article.find(params[:id])
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article was successfully deleted."
+  redirect_to articles_path
 end
 
 private
